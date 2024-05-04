ede6b195655b5c16ef28db56d862c215e2cdce9c
ManarSholi <60700125+ManarSholi@users.noreply.github.com>
2024-05-04 18:07:09 +0300

    borders

---
+++ borders/repeat/borderImageRepeat.js
@@ -0,0 +1,6 @@
+const options = document.querySelector('select');
+const element = document.querySelector('.my-element');
+
+options.addEventListener('change', () => {
+    element.style.borderImageRepeat = options.value;
+});
